9c1db3d4c128da6a3e1cf89fe5f1874c9471221e
Alex <alexdclase@gmail.com>
2020-05-16 17:12:29 +0200

    actualizacion

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,15 +7,61 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    
+    # CASO USUARIO PIEDRA
+    if options[player] == options[0]:
+        
+        # CASO AI PIEDRA
+        if options[ai] == options[0]:
+            return 'Empate!'
+
+        #CASO AI PAPEL
+        elif options[ai] == options[1]:
+            return 'Perdiste!'
+
+        #CASO AI TIJERA
+        else:
+            return 'Ganaste!'
+
+
+    # CASO USUARIO PAPEL
+    if options[player] == options[1]:
+        
+        # CASO AI PIEDRA
+        if options[ai] == options[0]:
+            return 'Ganaste!'
+
+        #CASO AI PAPEL
+        elif options[ai] == options[1]:
+            return 'Empate!'
+
+        #CASO AI TIJERA
+        else:
+            return 'Perdiste!'
+    
+
+    # CASO USUARIO TIJERA
+    if options[player] == options[2]:
+        
+        # CASO AI PIEDRA
+        if options[ai] == options[0]:
+            return 'Perdiste!'
+
+        #CASO AI PAPEL
+        elif options[ai] == options[1]:
+            return 'Ganaste!'
+
+        #CASO AI TIJERA
+        else:
+            return 'Empate!'
+        
 
 # Entry Point
 def Game():
-    #
-    #
-    
-    #
-    #
+
+    player = input('Introduce el número que quieras jugar (0.PIEDRA, 1.PAPEL, 2.TIJERA): ')
+
+    ai = random.randint(0, 3)
     
     winner = quienGana(player, ai)
 
